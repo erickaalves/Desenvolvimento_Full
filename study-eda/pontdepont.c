bffa5135dc944bb6fb3fd0ba2369f653e20177eb
[erickaalves] <[erickdosantos53@gmail.com]>
2025-06-04 14:25:14 -0300

    ponteiro de ponteiro treinamento

---
+++ study-eda/pontdepont.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int a =10;
+    int *p1 = &a;
+    int **p2 = &p1;
+
+    printf("&a = %p, a = %d\n", &a, a);
+    printf("&p1 = %p, *p1 = %d\n", &p1, *p1);
+    printf("&p2= %p, *p2 = %p\n", &p2, *p2);
+
+}
