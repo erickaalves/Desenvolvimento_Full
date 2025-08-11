db719412944270e59157cef8ae20101a15e68de8
[erickaalves] <[erickdosantos53@gmail.com]>
2025-08-11 17:36:30 -0300

    aritmetica dos vetores

---
+++ study-eda/aritmetica.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int v[5] = {10,5,4,16,1};
+    printf ("&v = %p, *v = %d", &v, v);
+
+    for (int i = 0; i<5; i++){
+        printf("&v[%d] = %p, v[%d] = %d\n", i, &v[i], i, v[i]);
+        printf("(v + %d) = %p, *(v + %d) = %d\n\n", i, (v + i), i, *(v + i));
+    }
+    return 0;
+}
