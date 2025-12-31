f3ac07e6be47d862535b09aef7459f73f5d26396
[erickaalves] <[erickdosantos53@gmail.com]>
2025-12-31 10:30:30 -0300

    [update] inclusão de CRUD inicial, faltando somente delete

---
+++ study-eda/crudstruct.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _livro {
+    char titulo[100];
+    unsigned int num_paginas;
+    float preco;
+}Livro;
+
+//construtor para livro
+Livro* create_livro(const char *titulo, unsigned int num_paginas, float preco){
+    Livro* livro =(Livro*) calloc(1,sizeof(Livro));
+    strcpy(livro -> titulo, titulo);
+    livro -> num_paginas = num_paginas;
+    livro -> preco = preco;
+    return livro;
+}
+void update_livro(Livro *livro, const char *titulo, unsigned int num_paginas, float preco){
+    strcpy (livro->titulo, titulo);
+    livro -> num_paginas = num_paginas;
+    livro -> preco = preco;
+
+}
+
+void print_livro(const Livro *livro){
+    printf("Titulo: %s\n", livro->titulo);
+    printf("Num de paginas: %d\n", livro->num_paginas);
+    printf("Preco: %.2f\n\n", livro->preco);
+}
+ void delete_livro (Livro* livro){
+    free(livro);
+    livro = NULL;
+ }
+
+int main(){
+    Livro *livro_harry = create_livro("Harry Poter 1", 200,25);
+    print_livro(livro_harry);
+    livro_harry->preco = 10;
+    print_livro(livro_harry);
+    update_livro(livro_harry, "Harry Poter 001", 210,30);
+    print_livro(livro_harry);
+    delete_livro(livro_harry);
+    print_livro(livro_harry);
+
+    return 0;
+}
